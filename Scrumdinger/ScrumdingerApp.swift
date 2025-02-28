d85c6bac1d8909abc2d5d9fa80a1ef9a818fb911
Abdul Wahab Khan <awkhan313@gmail.com>
2025-02-28 15:34:34 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Abdul Wahab Khan on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Abdul Wahab Khan on 28/02/2025.
+//
+
+import Testing
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
